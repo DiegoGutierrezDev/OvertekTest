7ffdf79af371ae7d01074da77c239eb5a8bde424
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2024-11-06 15:17:27 +0100

    Little fixes and setting up the table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+    <template>
+    <v-table theme="dark">
+      <thead>
+        <tr>
+          <th class="text-left">
+            Nombre del empleado
+          </th>
+          <th class="text-left">
+            Edad
+          </th>
+          <th class="text-left">
+            Salario
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.age }}</td>
+          <td>{{ employee.salary }}</td>
+        </tr>
+      </tbody>
+    </v-table>
 </template>
+
+<script setup>
+</script>
